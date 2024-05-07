ea32eb6e0aad04c5f955cf1a6bdcdcd30516a62b
zoro-2003 <117997042+zoro-2003@users.noreply.github.com>
2024-05-07 21:36:35 +0530

    Update ds9.r

--- ds9.r
+++ ds9.r
@@ -1,13 +1,11 @@
 library(ggplot2)
 library(cluster)
-
+library(factoextra)  
 data(iris)
-
-set.seed(123)  
-kmeans_result <- kmeans(iris[,1:4], centers = 3)
-
+fviz_nbclust(iris[, 1:4], kmeans, method = "silhouette") + labs(title = "Elbow Method for Determining Optimal Number of Clusters")
+set.seed(123)
+kmeans_result <- kmeans(iris[, 1:4], centers = 3)
 iris$Cluster <- as.factor(kmeans_result$cluster)
+ggplot(iris, aes(x = Petal.Length, y = Petal.Width, color = Cluster)) + geom_point() + labs(color = "Cluster")
 
-ggplot(iris, aes(x = Petal.Length, y = Petal.Width, color = Cluster)) +
-  geom_point() +
-  labs(color = "Cluster")
+
