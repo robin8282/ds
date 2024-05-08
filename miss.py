6c7d3e5506b4784e041e242f9d7c0a938c7d7a67
zoro-2003 <117997042+zoro-2003@users.noreply.github.com>
2024-05-08 07:11:03 +0530

    Create miss.py

---
+++ miss.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# using means
+
+data = {'A': [1, 2, None, 4, 5],
+'B': [10, None, 30, 40, 50]}
+df = pd.DataFrame(data)
+mean_A = df['A'].mean()
+mean_B = df['B'].mean()
+df_imputed = df.fillna({'A': mean_A, 'B': mean_B})
+
+
+# categorical method
+
+data = {'Category': ['A', 'B', None, 'A', 'C']}
+df =pd.DataFrame(data)
+df_filled = df.fillna({'Category': 'Unknown'})
+
+
+# interpolation
+data = {'Time': [1, 2, None, 4, 5],
+'Value': [10, 20, None, 40, 50]}
+df = pd.DataFrame(data)
+df_interpolated = df.interpolate()
