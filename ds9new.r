245ea8876bc624b85da453062ec6c5d643acc17a
zoro-2003 <117997042+zoro-2003@users.noreply.github.com>
2024-05-08 09:26:25 +0530

    Create ds9new.r

---
+++ ds9new.r
@@ -0,0 +1,12 @@
+library(factoextra)
+library(ggplot2)
+library(cluster)
+
+fviz_nbclust(iris[1:4], kmeans, method = "wss")
+
+set.seed(123)
+kmeans_res <- kmeans(iris[,1:4], centers = 3)
+summary (kmeans_res)
+iris$cluster <- as. factor (kmeans_res$cluster)
+
+fviz_cluster(kmeans_res, data = iris[,1:4])
