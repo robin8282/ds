47fc41103f1433faa5ec2b02495c654009ab5701
zoro-2003 <117997042+zoro-2003@users.noreply.github.com>
2024-05-08 09:52:14 +0530

    Update ds9new.r

--- ds9new.r
+++ ds9new.r
@@ -6,7 +6,7 @@ fviz_nbclust(iris[1:4], kmeans, method = "wss")
 
 set.seed(123)
 kmeans_res <- kmeans(iris[,1:4], centers = 3)
-summary (kmeans_res)
-iris$cluster <- as. factor (kmeans_res$cluster)
+summary(kmeans_res)
+iris$cluster <- as.factor (kmeans_res$cluster)
 
 fviz_cluster(kmeans_res, data = iris[,1:4])
